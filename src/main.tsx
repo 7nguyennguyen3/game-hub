270ef46d117168fe4ef65e0989bd11c7729a7c4e
Nguyen <7nguyennguyen3@gmail.com>
2023-12-24 20:23:47 -0800

    Handle errors

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-
 import theme from "./theme.ts";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
